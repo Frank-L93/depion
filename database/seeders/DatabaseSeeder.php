038a6448457cf211a7da0a6ab8ff5acfca67e215
Frank-L93 <frank.lambregts@outlook.com>
2023-10-09 07:26:14 +0200

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+        DB::table('configs')->insert([
+            'RoundsBetween_Bye' => 5,
+            'RoundsBetween' => 5,
+            'Club' => 5,
+            'Personal' => 5,
+            'Bye' => 5,
+            'Presence' => 5,
+            'Other' => 5,
+            'Start' => 50,
+            'Step' => 1,
+            'Name' => 'De Pion',
+            'Season' => '2023-2024',
+            'Admin' => 1,
+            'EndSeason' => 0,
+            'announcement' => 'Nieuw seizoen wordt opgestart',
+            'AbsenceMax' => 3,
+            'SeasonPart' => 13,
+            'presenceOrLoss' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "Frank Lambregts",
+            "password" => Hash::make('MagiStraal93!'),
+            "api_token" => "z8TdkiFKGz",
+            "email" => "frank@franklambregts",
+            "knsb_id" => 8090687,
+            "rechten" => 2,
+            "remember_token" => "Ebkmu3Rm1Ka2QNYTyPzryIcDbbL0jgb3WL8uh98P36zOkcdJz8DfdtfB8mJ2",
+            "rating" => 1876,
+            "beschikbaar" => 1,
+            "firsttimelogin" => 0,
+            "settings" => "{\"rss\": \"1\", \"games\": \"0\", \"layout\": \"app\", \"ranking\": \"1\", \"language\": \"nl\", \"notifications\": \"2\"}"
+        ]);
     }
 }
