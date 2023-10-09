5b28b4758e9a9f2241c33c11f4a448bc8e27c724
Frank Lambregts <frank@franklambregts.com>
2023-10-09 20:16:55 +0200

    taaltechnisch

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben je een reset-link gestuurd per mail. Kijk ook in je spambox!',
+    'throttled' => 'Wacht even met opnieuw proberen.',
+    'token' => 'De reset token is ongeldig.',
+    'user' => "We kunnen geen gebruiker vinden met dat e-mailadres. Vraag even na welk e-mailadres je moet gebruiken of laat het wijzigen.",
+
+];
